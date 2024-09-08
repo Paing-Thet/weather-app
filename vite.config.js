8041fbc4e924bef1e64c22df4e44398eda064fdd
Paing Thet <haixian.hex@gmail.com>
2024-09-09 06:47:28 +0700

    prepare to deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
   base: 'https://github.com/Paing-Thet/weather-app', // Change this to match your repo name
-  plugins: [vue(), vueJsx()],
+  plugins: [vue()],
 });
