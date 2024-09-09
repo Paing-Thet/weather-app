f09853384c5c96f1212862f610628edbfdd6e41a
Paing Thet <haixian.hex@gmail.com>
2024-09-10 02:40:34 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/Paing-Thet/weather-app',
+  base: '/weather-app/',
   plugins: [vue()],
 });
