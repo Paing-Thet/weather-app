2fdbe69c5fea420bff4ad2dbb904c3175c34c587
Paing Thet <haixian.hex@gmail.com>
2024-09-11 07:07:20 +0700

    step 11

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,44 @@
 <script>
 import axios from 'axios';
+import WeatherDisplay from './components/WeatherDisplay.vue';
+import VueSimpleSpinner from 'vue-simple-spinner';
+
+
 
 export default {
+  components: {
+    WeatherDisplay,
+    VueSimpleSpinner,
+  },
   data() {
     return {
       city: "",
       weather: null,
-      // loading: false,
-      // error: null,
+      errorMessage: null,
+      isLoading: false,
     };
   },
   methods: {
     async getWeatherData() {
-      // const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
-      // const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${apiKey}`;
-
-      // this.loading = true;
-      // this.error = null;
-      // this.weather = null;
-
+      this.isLoading = true;
       try {
         const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${apiKey}`);
-        // const response = await axios.get(url);
+        if(!response.ok){
+          throw new Error(`${this.city} City is not found`);
+        }
+
         const data = await response.json();
         this.weather = data;
-        this.loading = false; 
-      } catch (err) {
-        if(err.response && err.response.status === 404){
-          this.error = `${this.city} City is not found or It is lost city. Why not try again with different city`;
-        } else {
-          this.error = "Failed to fetch weather data"; 
-        }
-        this.loading = false;
+        this.errorMessage = null;
+
+      } catch (error) {
+
+        this.weather = null;
+        this.errorMessage = error.message;
+
+      } finally {
+        this.isLoading = false;
       }
     },
   },
@@ -40,21 +46,19 @@ export default {
 </script>
 
 <template>
-  <div id="app">
+  <div id="app" class="text-center">
     <h1 class="main-title">Weather App</h1>
 
     <input v-model="city" placeholder="Enter City Name" />
     <button @click="getWeatherData">Get Weather</button>
 
-    <div v-if="loading" class="spinner"></div>
-
-    <div v-if="!loading && weather">
-      <h2>{{ weather.name }}</h2>
-      <p>Temperature : {{ weather.main.temp }}°C</p>
-      <p>Condition : {{ weather.weather[0].description }}</p>
-      <p>Humidity : {{ weather.main.humidity }} %</p>
-      <p>Wind Speed : {{ weather.wind.speed }} m/s</p>
+    <div v-if="errorMessage" class="error-message">
+      <p>{{ errorMessage }}</p>
     </div>
+
+    <!-- <vue-simple-spinner v-if="isLoading" size="40" color="#42b883" /> -->
+
+    <WeatherDisplay v-else-if="weather && weather.main" :weather="weather" />
 
   </div>
 </template>
@@ -85,6 +89,11 @@ p {
   width: 40px;
   height: 40px;
   animation: spin 1s linear infinite;
+}
+
+.error-message {
+  color: red;
+  font-weight: bold;
 }
 
 @keyframes spin {
---
+++ src/components/WeatherDisplay.vue
@@ -0,0 +1,20 @@
+<script>
+    export default {
+        props: {
+            weather: {
+                type: Object,
+                required: true,
+            }
+        }
+    }
+</script>
+
+<template>
+    <div>
+        <h2>{{ weather.name }}</h2>
+        <p>Temperature : {{ weather.main.temp }}°C</p>
+        <p>Condition : {{ weather.weather[0].description }}</p>
+        <p>Humidity : {{ weather.main.humidity }} %</p>
+        <p>Wind Speed : {{ weather.wind.speed }} m/s</p>
+    </div>
+</template>
